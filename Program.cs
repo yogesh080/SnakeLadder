b06a7c8205f08cb4d52016886604124e8230ccaf
Yogesh <yogeshbietjhs@gmail.com>
2022-06-17 00:13:26 +0530

    inicial commit for dice roll

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and Ladder game");
+            Random random = new Random();
+            int dice1 = random.Next(1, 6);
+            Console.WriteLine("dice rolled we get:" + dice1);
+        }
+    }
+}
